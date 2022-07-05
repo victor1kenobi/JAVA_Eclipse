157afff8a5d3f0834458234a7a1b6e2c8bf0169e
victor1kenobi <vmsanchez7@gmail.com>
2022-07-06 00:40:36 +0200

    variables class añadida

---
+++ src/variables.java
@@ -0,0 +1,22 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+
+		byte edad = 35; //declaracion e iniciacion de variable en una unica linea
+		
+		/* la siguiente instruccion imprime en la consola System.out 
+		 * el valor de la variable edad
+		 */
+
+		System.out.println(edad);
+		
+		
+		edad = 75;	
+		
+		System.out.println(edad);
+		
+
+	}
+
+}
