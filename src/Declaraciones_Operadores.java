19e1ea28ceccf5d18cd76800ae3ef19cbefbb372
victor1kenobi <vmsanchez7@gmail.com>
2022-07-06 23:49:52 +0200

    declaracion operadores commit

---
+++ src/Declaraciones_Operadores.java
@@ -0,0 +1,25 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 5;
+		
+		double b;
+		
+		b = 7;
+		
+		double c = b / a;
+		
+		//c++;
+		
+		//c+=7;
+		
+		//c-=5;
+		
+		System.out.println(c);	
+
+	}
+
+}
