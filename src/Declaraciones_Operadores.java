333d3a93d454d7a166b721b6f897ac834daaf844
victor1kenobi <vmsanchez7@gmail.com>
2022-07-07 23:57:39 +0200

    añaddo ejemplo de constantes

--- src/Declaraciones_Operadores.java
+++ src/Declaraciones_Operadores.java
@@ -2,9 +2,10 @@
 public class Declaraciones_Operadores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		double a = 5;
+		// CALCULOS
+		
+		/*double a = 5;
 		
 		double b;
 		
@@ -12,13 +13,40 @@ public class Declaraciones_Operadores {
 		
 		double c = b / a;
 		
-		//c++;
+		c++;
 		
-		//c+=7;
+		c+=7;
 		
-		//c-=5;
+		c-=5;*/
 		
-		System.out.println(c);	
+		//CONSTANTES
+		
+		/*final int c = 7;
+		
+		c = 9;
+		*/
+		
+		//CONVERSION A PULGADAS
+		/*
+		final double apulgadas = 2.54;
+		
+		double cm = 6;
+		
+		double resultado = cm / apulgadas;
+		
+		
+		System.out.println("En " + cm + " centimetros hay " + resultado + " pulgadas" );	
+		*/
+		
+		int operador1, operador2, resultado;
+		
+		operador1 = 8;
+		
+		operador2 = 6;
+		
+		resultado = operador1 + operador2;
+		
+		System.out.println(resultado);
 
 	}
 
